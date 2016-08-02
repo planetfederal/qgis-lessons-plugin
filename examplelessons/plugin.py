117d5e9af9dbc98d77edc9113ab1fa22cc218272
volaya <volayaf@gmail.com>
2016-08-02 09:23:27 +0200

    use addLessonsFolder to add example lessons

--- examplelessons/plugin.py
+++ examplelessons/plugin.py
@@ -3,41 +3,21 @@
 # (c) 2016 Boundless, http://boundlessgeo.com
 # This code is licensed under the GPL 2.0 license.
 #
-import glob
 import os
 
 class LessonsCollection:
 
 	def __init__(self, iface):
 		try:
-			from lessons import addLessonModule
+			from lessons import addLessonsFolder
 		except:
 			return
 
 		folder = os.path.join(os.path.dirname(__file__), "_lessons")
-		def isPackage(d):
-			d = os.path.join(folder, d)
-			return os.path.isdir(d) and glob.glob(os.path.join(d, '__init__.py*'))
-		packages = filter(isPackage, os.listdir(folder))
-		for p in packages:
-			m = __import__(__name__.split(".")[0] + "._lessons." + p, fromlist="dummy")
-			addLessonModule(m)
-
-		# add tests to test plugin
-		try:
-			from qgistester.tests import addTestModule
-			from examplelessons.test import testplugin
-			addTestModule(testplugin, "Example lessons")
-		except Exception as e:
-			pass
+		addLessonsFolder(folder)
 
 	def unload(self):
-		try:
-			from qgistester.tests import removeTestModule
-			from examplelessons.test import testplugin
-			removeTestModule(testplugin, "Example lessons")
-		except Exception as e:
-			pass
+		pass
 
 	def initGui(self):
 		pass
