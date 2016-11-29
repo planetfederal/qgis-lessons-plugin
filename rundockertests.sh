fb69b7bbc60a9091376e3706c8ff9eb94d88c442
Alexander Bruy <alexander.bruy@gmail.com>
2016-11-29 12:18:55 +0200

    update rundockertests.sh

--- rundockertests.sh
+++ rundockertests.sh
@@ -4,25 +4,30 @@
 xhost +
 
 PLUGIN_NAME="lessons"
+# One of master_2, master, release
+TARGET_VERSION=master
 
-docker rm -f qgis-testing-environment
+if [ $TARGET_VERSION = 'master' ]; then
+    PIP_EXECUTABLE=pip3
+else
+    PIP_EXECUTABLE=pip
+fi
 
-# replace release with master or master_2 if you wish to test on master or master_2
-docker pull elpaso/qgis-testing-environment:release
-docker tag elpaso/qgis-testing-environment:release qgis-testing-environment
+#docker rm -f qgis-testing-environment
+
+# replace latest with master if you wish to test on master, latest is
+# latest supported Boundless release
+docker pull boundlessgeo/qgis-testing-environment:$TARGET_VERSION
+docker tag boundlessgeo/qgis-testing-environment:$TARGET_VERSION qgis-testing-environment
 
 docker run -d  --name qgis-testing-environment  -e DISPLAY=:99 -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/tests_directory qgis-testing-environment
 
-
+# Setup
 docker exec -it qgis-testing-environment sh -c "qgis_setup.sh $PLUGIN_NAME"
+docker exec -it qgis-testing-environment sh -c "$PIP_EXECUTABLE install paver"
 
-# Install the plugin
-docker exec -it qgis-testing-environment sh -c "easy_install --upgrade pip"
-docker exec -it qgis-testing-environment sh -c "pip install paver"
-docker exec -it qgis-testing-environment sh -c "cd /tests_directory && paver setup"
-docker exec -it qgis-testing-environment sh -c "mkdir -p /root/.qgis2/python/plugins/"
-docker exec -it qgis-testing-environment sh -c "ln -s /tests_directory/$PLUGIN_NAME /root/.qgis2/python/plugins/$PLUGIN_NAME"
+# Package
+docker exec -it qgis-testing-environment sh -c "cd /tests_directory && paver setup && paver package --tests"
 
-
-# run the tests
-docker exec -it qgis-testing-environment sh -c "DISPLAY=unix:0 qgis_testrunner.sh $PLUGIN_NAME.tests.unit_tests"
+# Run tests
+docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh ${PLUGIN_NAME}.tests.testerplugin.run_tests"
