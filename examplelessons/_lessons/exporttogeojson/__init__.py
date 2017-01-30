60d6dc900bc89b0f99a2e15f3e725c3d749c3fe8
Alexander Bruy <alexander.bruy@gmail.com>
2017-01-30 16:29:51 +0200

    fix formatting

--- examplelessons/_lessons/exporttogeojson/__init__.py
+++ examplelessons/_lessons/exporttogeojson/__init__.py
@@ -10,7 +10,9 @@ from lessons import addLessonModule
 
 lesson = Lesson("Export to geojson", "Basic lessons", "lesson.html")
 lesson.addStep("Set 'points' layer as active layer", "activelayer.md",
-               function=lambda: setActiveLayer("points"), endcheck=lambda:layerActive("points"), steptype=Step.MANUALSTEP)
+               function=lambda: setActiveLayer("points"),
+               endcheck=lambda:layerActive("points"),
+               steptype=Step.MANUALSTEP)
 lesson.addMenuClickStep("Layer/Save As...")
 lesson.addStep("Save the file as geojson", "saveas.html", steptype=Step.MANUALSTEP)
 lesson.addNextLesson("Basic lessons", "Export to geojson (yaml example)")
